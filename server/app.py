fb40f288e289050114ef127e92227d3cde4538eb
Abed <ai1138@nyu.edu>
2021-04-24 15:56:50 -0400

    yessir

--- server/app.py
+++ server/app.py
@@ -6,4 +6,6 @@ app = Flask(__name__)
 @app.route('/time')
 def get_current_time():
     return {'time': time.time()}
-    app.run('127.0.0.1', 5000, debug = True)
+    app.run('127.0.0.1', 5000, debug = True
+
+#commit test

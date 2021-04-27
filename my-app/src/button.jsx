4cc8181afd50bd764fbef63d5df441df8da3a75d
Hellen Ekpo <he560@nyu.edu>
2021-04-26 23:38:19 -0400

    the new pages

--- my-app/src/button.jsx
+++ my-app/src/button.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import "./button.css";
+
+
+function onClick() {
+    window.location.href="http://google.com";
+}
+
 export const Button = ({children, type, onClick, buttonStyle, buttonSize}) => {
     return (<button onClick = {onClick} type = {type}> {children}</button>
     ); };
--- my-app/src/home.js
+++ my-app/src/home.js
@@ -2,21 +2,27 @@ import React from 'react';
 import Table from './Table'
 import SearchPage from './SearchPage'
 import {button} from "./button";
+import Button from '@material-ui/core/Button';
+import FormPage from './FormPage'
+
  
 const home = () => {
     return (
+	
+ 
        <div style = {{display: "flex", flexDirection: "column",
         alignItems: "center",
         justifyContent: "center"}}>
         
-          <h1>Welcome Aboard!</h1>  
-          <button>Log In/Register</button>
+          <h1>Welcome Aboard!</h1> 
+          <Button variant="contained" color="secondary" href= './FormPage'>Log In/Register</Button>
           <tr>At your convenience, anytime, anywhere</tr>
           <SearchPage/>
           <Table />
        </div>
     );
 }
- 
+
+
 export default home;
 

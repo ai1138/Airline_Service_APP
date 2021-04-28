da32376ee85818167979164c603ba74b8d0fb2ba
Abed <ai1138@nyu.edu>
2021-04-28 01:31:17 -0400

    table works

--- server/app.py
+++ server/app.py
@@ -1,29 +1,43 @@
 from flask import Flask
-'''from flask import jsonify
-import pymysql.cursors '''
+from flask import jsonify
+import pymysql.cursors 
+from flask_cors import CORS
 app = Flask(__name__)
+CORS(app)
  
-'''conn = pymysql.connect(host='localhost',
+conn = pymysql.connect(host='localhost',
                        user='root',
                        password='',
                        db='flights',
                        charset='utf8mb4',
                        cursorclass=pymysql.cursors.DictCursor)
 
-@app.route('/flights',methods=['GET'])
+class user():
+    def __init__(self,id,role,token):
+        self.id = id
+        self.role = role
+        self.token = token
+@app.route('/loginAuth',methods=['GET'])
+def loginAuth():
+    pass
+@app.route('/flights-table',methods=['GET'])
 def getFlights():
     cursor = conn.cursor()
     query = 'SELECT flight_num,statuses,departure_date_and_time,departure_airport,airport_arrival,arrival_date_and_time FROM flight'
     cursor.execute(query)
     data = cursor.fetchall()
     data = jsonify(data)
-    return data'''
-@app.route('/home',methods=['GET'])
+    return data
+@app.route('/home',methods = ['GET'])
 def home():
     return "hello"
-@app.route('/')
+
+@app.route('/flights-searchFlight',methods = ['Post'])
+def searchDb():
+    data = request.get_json()
+    
+
 def index():
    return "WELCOME!!! This is the home page"
-
 if __name__ == "__main__":
 	app.run(host="localhost",port = 5000, debug=True,threaded = True)

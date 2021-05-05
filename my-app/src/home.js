571c4a648fcb3ae745450f3ecd09cdf3d67c0247
sheikaislam <si965@nyu.edu>
2021-05-04 21:26:11 -0400

    added stuff

--- my-app/src/home.js
+++ my-app/src/home.js
@@ -4,6 +4,7 @@ import Table from './Table'
 import SearchPage from './SearchPage'
 import {button} from "./button";
 import Button from '@material-ui/core/Button';
+import logo from './PngItem_61922.png';
 
 
  
@@ -14,7 +15,7 @@ const home = () => {
        <div style = {{display: "flex", flexDirection: "column",
         alignItems: "center",
         justifyContent: "center"}}>
-        
+          <img src = {logo} alt = "Logo" height="200" width="200" />
           <h1>Welcome Aboard!</h1> 
           <Link variant="contained" color="secondary" to= '/lspage'>Log In/Register</Link>
           <tr>At your convenience, anytime, anywhere</tr>
